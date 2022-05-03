8fb0fa20fd8ad8a718ef0a647ddc6e70d1986ca9
mrcaseb <roemerle@gmail.com>
2022-05-03 20:32:58 +0200

    catch failing arrow for now

--- data-raw/release_otc_contract_data.R
+++ data-raw/release_otc_contract_data.R
@@ -10,15 +10,17 @@ save <- dplyr::left_join(contracts, player_details, by = c("player_page" = "play
 save_dir <- tempdir()
 
 cli::cli_alert_info("Save Files in working directory {.path {save_dir}}...")
-saveRDS(save, paste0(save_dir, "/historical_contracts.rds"))
-readr::write_csv(save, paste0(save_dir, "/historical_contracts.csv.gz"))
-arrow::write_parquet(save, paste0(save_dir, "/historical_contracts.parquet"))
-qs::qsave(save, paste0(save_dir, "/historical_contracts.qs"),
-          preset = "custom",
-          algorithm = "zstd_stream",
-          compress_level = 22,
-          shuffle_control = 15
-)
+try({
+  saveRDS(save, file.path(save_dir, "historical_contracts.rds"))
+  readr::write_csv(save, file.path(save_dir, "historical_contracts.csv.gz"))
+  arrow::write_parquet(save, file.path(save_dir, "historical_contracts.parquet"))
+  qs::qsave(save, file.path(save_dir, "historical_contracts.qs"),
+            preset = "custom",
+            algorithm = "zstd_stream",
+            compress_level = 22,
+            shuffle_control = 15
+  )
+})
 
 to_upload <- list.files(path = save_dir, full.names = TRUE)
 to_upload <- to_upload[stringr::str_detect(to_upload, "historical")]
