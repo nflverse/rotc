2cecaf7611ab21dcc9a1df71e6167fcc141f9f4d
Tan-DESKPC <tan@tanho.ca>
2022-05-29 11:38:19 -0400

    use nflversedata::nflverse_save() for consistency

--- data-raw/release_otc_contract_data.R
+++ data-raw/release_otc_contract_data.R
@@ -7,29 +7,13 @@ contracts <- rotc::otc_historical_contracts_all()
 
 save <- dplyr::left_join(contracts, player_details, by = c("player_page" = "player_url"))
 
-save_dir <- tempdir()
+options(piggyback.verbose = FALSE)
 
-cli::cli_alert_info("Save Files in working directory {.path {save_dir}}...")
-try({
-  saveRDS(save, file.path(save_dir, "historical_contracts.rds"))
-  readr::write_csv(save, file.path(save_dir, "historical_contracts.csv.gz"))
-  qs::qsave(save, file.path(save_dir, "historical_contracts.qs"),
-            preset = "custom",
-            algorithm = "zstd_stream",
-            compress_level = 22,
-            shuffle_control = 15
-  )
-  arrow::write_parquet(save, file.path(save_dir, "historical_contracts.parquet"))
-})
-
-to_upload <- list.files(path = save_dir, full.names = TRUE)
-to_upload <- to_upload[stringr::str_detect(to_upload, "historical")]
-
-cli::cli_ul("Will release {.path {to_upload}}")
-
-nflversedata::nflverse_upload(to_upload, "contracts")
-
-cli::cli_alert_info("Remove Temporary Directory")
-unlink(save_dir, recursive = TRUE)
+nflversedata::nflverse_save(
+  data_frame = save,
+  file_name = "historical_contracts",
+  nflverse_type = "Historical Contract Data from OverTheCap.com",
+  release_tag = "contracts"
+)
 
 cli::cli_alert_success("DONE!")
