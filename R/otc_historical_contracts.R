5f8e52eca6b94548f1baca5d2d1853a1fa81a07e
mrcaseb <mrcaseb@gmail.com>
2023-03-10 22:56:18 +0100

    fix na_if behavior in new dplyr

--- R/otc_historical_contracts.R
+++ R/otc_historical_contracts.R
@@ -46,7 +46,7 @@ otc_historical_contracts <- function(position = c("QB", "RB", "FB", "WR",
       ),
       .fns = readr::parse_number
     )) %>%
-    dplyr::na_if("") %>%
+    dplyr::mutate_if(is.character, dplyr::na_if) %>%
     dplyr::rename(apy_cap_pct = apy_as_percent_of_cap_at_signing) %>%
     dplyr::mutate(
       apy_cap_pct = apy_cap_pct / 100,
